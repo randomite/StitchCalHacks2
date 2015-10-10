f1066d62862446ff221b2e1a9db035b7122d6b90
Ravin Sardal <ravin.sardal@gmail.com>
2015-10-10 01:31:21 -0700

    Added Explore VC

---
+++ Stitch/Stitch/ExploreViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ExploreViewController.swift
+//  Stitch
+//
+//  Created by Ravin Sardal on 10/10/15.
+//  Copyright © 2015 Ravin Sardal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExploreViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
