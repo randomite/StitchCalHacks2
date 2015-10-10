e80c1301ee82fa178824de2fa0e139be423cc9b7
Ravin Sardal <ravin.sardal@gmail.com>
2015-10-10 00:54:20 -0700

    Created Login View

--- Stitch/Stitch/ViewController.swift
+++ Stitch/Stitch/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var logoImageView: UIImageView!
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.scrollView.userInteractionEnabled = true;
+        self.scrollView.contentSize = CGSizeMake(375, 400)
+        self.scrollView.flashScrollIndicators()
+        self.userNameTextField.delegate = self
+        self.passwordTextField.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        self.scrollView.setContentOffset(CGPointMake(0.0, 200.0), animated: true)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        self.scrollView.setContentOffset(CGPointMake(0.0, 0.0), animated: true)
+    }
 
 }
 
