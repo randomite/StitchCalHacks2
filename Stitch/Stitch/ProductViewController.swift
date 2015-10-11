6bb86bed59cfe9858db0f431f3083b412178ddc2
Ravin Sardal <ravin.sardal@gmail.com>
2015-10-10 18:49:44 -0700

    Added transistions to all VC's and added text to product VC

--- Stitch/Stitch/ProductViewController.swift
+++ Stitch/Stitch/ProductViewController.swift
@@ -19,6 +19,7 @@ class ProductViewController: UIViewController {
     @IBOutlet var leftSmallImageVIew: UIImageView!
     @IBOutlet var middleSmallImageView: UIImageView!
     @IBOutlet var rightSmallImageView: UIImageView!
+    @IBOutlet var descriptionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,6 +59,20 @@ class ProductViewController: UIViewController {
                 rightSmallImageView.image = UIImage(data: data)
                 rightSmallImageView.transform = CGAffineTransformMakeRotation(-0.50);
             }
+        }
+        
+        if(urlOfProduct.containsString("featured")){
+            self.descriptionTextView.text = "these are featured"
+        }else if(urlOfProduct.containsString("shirts")){
+            self.descriptionTextView.text = "these are shirts"
+        }else if(urlOfProduct.containsString("jackets")){
+            self.descriptionTextView.text = "these are jackets"
+        }else if(urlOfProduct.containsString("shoes")){
+            self.descriptionTextView.text = "these are shoes"
+        }else if(urlOfProduct.containsString("hats")){
+            self.descriptionTextView.text = "these are hats"
+        }else if(urlOfProduct.containsString("pants")){
+            self.descriptionTextView.text = "these are pants"
         }
     }
     
